b7bdf2b0f200d6c79450d4b199bc5261ef1f760a
thalmannpioneer <82112296+thalmannpioneer@users.noreply.github.com>
2026-03-16 09:46:35 +0300

    error window

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ const add_toggle = () => {
     dark.classList.toggle("dark__visible");
     add.classList.toggle("add__visible");
 };
+
+const error_toggle = () => {
+    error.classList.toggle("error__visible");
+}
